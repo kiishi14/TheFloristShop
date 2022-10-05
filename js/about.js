f3afc08f89f13aaa79eceb7ddcc9ef32573e77d3
kiishi14 <aladesuru441@gmail.com>
2022-10-05 01:00:26 +0100

    modal

--- js/about.js
+++ js/about.js
@@ -1,6 +1,9 @@
 const sidebarToggle = document.querySelector(".sidebar-toggle");
 const sidebarClose = document.querySelector(".sidebar-close-toggle");
 const sideBar = document.querySelector(".sidebar");
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modalContent = document.querySelector(".modal-content");
 
 sidebarToggle.addEventListener("click", function () {
   sideBar.classList.toggle("show-sidebar");
@@ -8,4 +11,12 @@ sidebarToggle.addEventListener("click", function () {
 
 sidebarClose.addEventListener("click", function () {
   sideBar.classList.remove("show-sidebar");
+});
+
+modalBtn.addEventListener("click", function () {
+  modalContent.classList.add("show-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+  modalContent.classList.remove("show-modal");
 });
