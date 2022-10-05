1e55a10b1503700cefc12a5095ea14c25dd4a8f1
kiishi14 <aladesuru441@gmail.com>
2022-10-05 23:36:46 +0100

    faq section

--- js/about.js
+++ js/about.js
@@ -4,6 +4,7 @@ const sideBar = document.querySelector(".sidebar");
 const modalBtn = document.querySelector(".modal-btn");
 const closeBtn = document.querySelector(".close-btn");
 const modalContent = document.querySelector(".modal-content");
+const questionBtns = document.querySelectorAll(".question-btn");
 
 sidebarToggle.addEventListener("click", function () {
   sideBar.classList.toggle("show-sidebar");
@@ -19,4 +20,11 @@ modalBtn.addEventListener("click", function () {
 
 closeBtn.addEventListener("click", function () {
   modalContent.classList.remove("show-modal");
+});
+
+questionBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (item) {
+    const question = item.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-answer");
+  });
 });
